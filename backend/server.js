dc160c4e738171341390f302aa3a5fade557f9a0
hananramss <hananramos19@gmail.com>
2024-03-28 00:10:44 +0800

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,11 @@ app.get('/', (req, res) => {
 });
 
 // Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Serve React app
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
 //Set up server
