f4fbee9ce5c741f87994bc5c62c47b6af3315c73
hananramss <hananramos19@gmail.com>
2024-03-27 15:28:39 +0800

    Added Code

---
+++ admin/src/utils/constant.js
@@ -0,0 +1,21 @@
+export const baseUrl = `https://dashretailhubbackend.onrender.com`;
+// export const salesUrl = "https://dashretailhubbackend.onrender.com/api/getAllsales";
+// export const expensesUrl = "https://dashretailhubbackend.onrender.com/api/getAllExpenses";
+// export const employeesUrl = "https://dashretailhubbackend.onrender.com/api/getAllEmployees";
+
+// export const productsUrl = "https://dashretailhubbackend.onrender.com/api/getAllProducts";
+// export const productsAddUrl = "https://dashretailhubbackend.onrender.com/api/createProducts";
+// export const productsUpdateUrl = "https://dashretailhubbackend.onrender.com/api/updateProducts";
+// export const productsDeleteUrl = "https://dashretailhubbackend.onrender.com/api/deleteProducts";
+
+// export const usersUrl = "https://dashretailhubbackend.onrender.com/api/getAllUsers";
+// export const usersAddUrl = "https://dashretailhubbackend.onrender.com/api/createUsers";
+// export const usersUpdateUrl = "https://dashretailhubbackend.onrender.com/api/updateUsers";
+// export const usersDeleteUrl = "https://dashretailhubbackend.onrender.com/api/deleteUsers";
+
+// export const ordersUrl = "https://dashretailhubbackend.onrender.com/api/getAllOrders";
+
+export const adminUrl = `https://dashretailhubbackend.onrender.com/api/getAllAdmin`;
+// export const registerAdmin = "https://dashretailhubbackend.onrender.com/api/registerAdmin";
+// export const loginAdmin = "https://dashretailhubbackend.onrender.com/api/loginAdmin";
+// export const logoutAdmin = "https://dashretailhubbackend.onrender.com/api/logoutAdmin"; //export const logoutAdmin = "http://localhost:5000/api/getAllUsers"; 
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cookieParser = require('cookie-parser');
+const cors = require("cors")
+require("dotenv").config()
+
+const dashboardRoutes = require('./routes/dashboardRoutes');
+
+// init app % middleware
+const app = express()
+app.use(express.json()) 
+
+// Enable CORS middleware
+app.use(cors({
+    origin: ["https://dashretailhub.onrender.com"],
+    credentials: true
+}));
+
+// db connection
+mongoose.connect(process.env.MONGO_URI)
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+db.once("open", () => {
+    console.log("Connected to MongoDB");
+});
+
+// Use your router for the specified routes
+app.use('/api', dashboardRoutes);
+app.use(cookieParser())
+
+// Define routes
+app.get('/', (req, res) => {
+    res.json({ message: 'Successfully deployed backend.' });
+});
+
+//Set up server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
